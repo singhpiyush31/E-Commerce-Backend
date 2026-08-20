2eee5125ba7ff3140ea2e249e78e5d50be7fcb0d
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-08-20 22:45:13 +0530

    Add endpoint to update category

--- controllers/category.js
+++ controllers/category.js
@@ -34,11 +34,35 @@ exports.createCategory = async (req, res) => {
 
 exports.getCategory = async (req,res) => {
     try {
-        const category = await Category.find().select("-user").sort({ _id: -1 });
+        const category = await Category.find()
+            .populate("user", "name email")
+            .populate("updatedBy", "name email")
+            .sort({ _id: -1 });
         let totalCategories = category.length;
 
         res.status(200).json({ message: "Categories: ", category, totalCategories });
     } catch (err) {
         res.status(500).json({ message: "Internal Server Error!", error: err.message });
+    }
+};
+
+exports.updateCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const { name, description } = req.body;
+        const updateCategory = await Category.findByIdAndUpdate(
+            categoryId,
+            { name, description, user: req.user._id },
+            { returnDocument: "after", runValidators: true },
+        )
+        if (!updateCategory) {
+            return res.status(404).json({ message: "Category not found!" });
+        }
+        res.status(200).json({ message: "Updated Category: ", updateCategory });
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error: err.message,
+        });
     }
 };
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { createCategory, getCategory } = require('../controllers/category');
+const { createCategory, getCategory, updateCategory } = require('../controllers/category');
 const { userAuth, isAdmin } = require('../middlewares/authentication');
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", userAuth, isAdmin, createCategory);
 categoryRouter.get("/", getCategory);
+categoryRouter.patch("/:id", userAuth, isAdmin, updateCategory);
 
 module.exports = categoryRouter;
