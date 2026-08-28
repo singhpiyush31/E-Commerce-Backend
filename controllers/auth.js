7bdd722e9beb77c515b16c52253d8c8035afd70c
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-08-28 10:55:54 +0530

    Add endpoint to fetch current user

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,3 +114,22 @@ exports.logout = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+exports.me = async (req, res) => {
+    try {
+        res.status(200).json({
+            message: "Current user",
+            user: {
+                _id: req.user._id,
+                name: req.user.name,
+                email: req.user.email,
+                role: req.user.role,
+            },
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: err.message,
+        });
+    }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, logout } = require('../controllers/auth');
+const { register, login, logout, me } = require('../controllers/auth');
+const { userAuth } = require('../middlewares/authentication');
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.get("/me", userAuth, me);
 
 module.exports = authRouter;
