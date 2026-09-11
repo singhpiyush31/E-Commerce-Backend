42c29018648e34e13c630e8f13854d9fcb33bd7e
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-09-11 22:45:46 +0530

    Add endpoint to clear cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -104,3 +104,27 @@ exports.getCart = async (req, res) => {
         });
     }
 };
+
+exports.clearCart = async (req, res) => {
+    try {
+        const loggedInUser = req.user._id;
+        const clearCart = await Cart.findOne({ user: loggedInUser });
+
+        if (!clearCart) {
+            return res.status(404).json({ message: "Cart not found!" });
+        }
+
+        clearCart.items = [];
+        await clearCart.save();
+
+        res.status(200).json({
+            message: "Cart cleared successfully!",
+            clearCart,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal Server Error!",
+            error: err.message,
+        });
+    }
+};
--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const { userAuth } = require("../middlewares/authentication");
-const { createCart, getCart } = require("../controllers/cart");
+const { createCart, getCart, clearCart } = require("../controllers/cart");
 
 const cartRouter = express.Router();
 
 cartRouter.post("/", userAuth, createCart);
 cartRouter.get("/", userAuth, getCart);
+cartRouter.delete("/", userAuth, clearCart);
 
 module.exports = cartRouter;
